e79b7f49b40a4c08142778fcf61287073646e5dd
Luciano <luciano01.dev@gmail.com>
2023-11-16 18:09:23 -0300

    Add equatable dependency and move mocktail to dev_dependency

--- lib/features/chuck_jokes/domain/entities/category_entity.dart
+++ lib/features/chuck_jokes/domain/entities/category_entity.dart
@@ -1,7 +1,14 @@
-class CategoryEntity {
+import 'package:equatable/equatable.dart';
+
+class CategoryEntity extends Equatable {
   final String category;
 
   const CategoryEntity({
     required this.category,
   });
+
+  @override
+  List<Object?> get props => [
+        category,
+      ];
 }
